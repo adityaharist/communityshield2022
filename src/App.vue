73bb3cdfd33cdf0aff82b08ac85e78cc026c9de8
Ari Firmansyah <aditya.harist@student.uny.ac.id>
2022-07-30 20:39:23 +0700

    commit proyek web community shield

---
+++ src/App.vue
@@ -0,0 +1,118 @@
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue'
+const todos = ref([])
+const name = ref('')
+const input_content = ref('')
+const input_category = ref(null)
+const todos_asc = computed(() => todos.value.sort((a,b) =>{
+	return a.createdAt - b.createdAt
+}))
+watch(name, (newVal) => {
+	localStorage.setItem('name', newVal)
+})
+watch(todos, (newVal) => {
+	localStorage.setItem('todos', JSON.stringify(newVal))
+}, {
+	deep: true
+})
+const addTodo = () => {
+	if (input_content.value.trim() === '' || input_category.value === null) {
+		return
+	}
+	todos.value.push({
+		content: input_content.value,
+		category: input_category.value,
+		done: false,
+		editable: false,
+		createdAt: new Date().getTime()
+	})
+}
+const removeTodo = (todo) => {
+	todos.value = todos.value.filter((t) => t !== todo)
+}
+onMounted(() => {
+	name.value = localStorage.getItem('name') || ''
+	todos.value = JSON.parse(localStorage.getItem('todos')) || []
+})
+</script>
+
+<template>
+	<main class="app">
+
+		<section class="greeting">
+			<h2 class="title">
+				Community Shield, <input type="text" id="name" placeholder="isi klub bola" v-model="name">
+			</h2>
+		</section>
+
+		<section class="create-todo">
+			<h3>Tulis pesan dibawah ini</h3>
+
+			<form id="new-todo-form" @submit.prevent="addTodo">
+				<h4>Apa pesanmu buat klub pilihanmu?</h4>
+				<input
+					type="text"
+					name="content"
+					id="content"
+					placeholder="e.g. Klub kebanggaanku pasti menang Comunity Shield"
+					v-model="input_content" />
+
+				<h4>Pilih klub</h4>
+				<div class="options">
+
+					<label>
+						<input
+							type="radio"
+							name="category"
+							id="category1"
+							value="klub1"
+							v-model="input_category" />
+						<span class="bubble klub1"></span>
+						<div>Liverpool FC</div>
+					</label>
+
+					<label>
+						<input
+							type="radio"
+							name="category"
+							id="category2"
+							value="klub2"
+							v-model="input_category" />
+						<span class="bubble klub2"></span>
+						<div>Manchester City</div>
+					</label>
+
+				</div>
+
+				<input type="submit" value="Submit pesanmu" />
+			</form>
+		</section>
+
+		<section class="todo-list">
+			<h3>PESAN FANS</h3>
+			<div class="list" id="todo-list">
+
+				<div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+					<label>
+						<input type="checkbox" v-model="todo.done" />
+						<span :class="`bubble ${
+							todo.category == 'klub1'
+								? 'klub1'
+								: 'klub2'
+						}`"></span>
+					</label>
+
+					<div class="todo-content">
+						<input type="text" v-model="todo.content" />
+					</div>
+
+					<div class="actions">
+						<button class="delete" @click="removeTodo(todo)">Delete</button>
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+	</main>
+</template>
